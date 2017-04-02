b6118bdc93b20a73eb5671672ba96da35a4ed360
jankrepl <jankrepl@85-195-198-59.init7.net>
2017-04-03 01:29:23 +0200

    Test commit - deleted one library

---
+++ Hackerrank/Algorithms/Warm_up/A_Very_Big_Sum.cpp
@@ -0,0 +1,24 @@
+// Name: A_Very_Big_Sum
+// WWWW: https://www.hackerrank.com/challenges/a-very-big-sum/submissions/code/36866139
+// Author: Jan Krepl
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    long long result = 0;
+    for(int arr_i = 0; arr_i < n; arr_i++){
+       cin >> arr[arr_i];
+        result += arr[arr_i];
+    }
+    cout << result;
+    return 0;
+}
---
+++ Hackerrank/Algorithms/Warm_up/Diagonal_Difference.cpp
@@ -0,0 +1,33 @@
+// Name: Diagonal_Difference
+// WWWW: https://www.hackerrank.com/challenges/diagonal-difference
+// Author: Jan Krepl
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    long ld = 0;
+    long rd = 0;
+    vector<vector<int> > a(n, vector<int>(n));
+    for(int a_i = 0; a_i < n; a_i++){
+       for(int a_j = 0; a_j < n; a_j++){
+          cin >> a[a_i][a_j];
+           if(a_i == a_j){
+               ld += a[a_i][a_j];
+           }
+           if(a_i == n - a_j - 1){
+               rd += a[a_i][a_j];
+           }
+       }
+    }
+    cout << abs(ld - rd);
+    return 0;
+}
