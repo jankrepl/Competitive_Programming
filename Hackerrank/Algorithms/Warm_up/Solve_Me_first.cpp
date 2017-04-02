465df505d7e88d67e2dd3b44dc128605ae23f03e
jankrepl <jankrepl@85-195-198-59.init7.net>
2017-04-03 00:54:22 +0200

    First commit

---
+++ Hackerrank/Algorithms/Warm_up/Solve_Me_first.cpp
@@ -0,0 +1,25 @@
+// Name: Solve_Me_First
+// WWWW: https://www.hackerrank.com/challenges/solve-me-first
+// Author: Jan Krepl
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int solveMeFirst(int a, int b) {
+    return a + b;
+  
+}
+
+int main(){
+  int num1, num2;
+  int sum;
+  cin >> num1 >> num2;
+  sum = solveMeFirst(num1,num2);
+  cout << sum;
+}
