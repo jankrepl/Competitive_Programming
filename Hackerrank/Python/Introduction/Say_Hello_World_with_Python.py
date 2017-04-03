d52260c465fd5259afaee36e54b2d34a2313b4ea
jankrepl <jankrepl@85-195-198-59.init7.net>
2017-04-03 02:26:34 +0200

    first commit

---
+++ Hackerrank/Python/Introduction/Say_Hello_World_with_Python.py
@@ -0,0 +1,6 @@
+# Name: Say_Hello_World_with_Python
+# WWWW: https://www.hackerrank.com/challenges/py-hello-world/submissions/code/39452682
+# Author: Jan Krepl
+
+if __name__ == '__main__':
+    print("Hello, World!")
