7ff8d3da2e6bd2eab43a610bbecdcf3d63b1d314
jankrepl <jankrepl@85-195-198-59.init7.net>
2017-04-03 02:08:21 +0200

    first commit

---
+++ SPOJ/001.cpp
@@ -0,0 +1,18 @@
+// Name: Life, the Universe, and Everything
+// ID: 1
+// Author: Jan Krepl
+
+#include<iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int x;
+    while(1){
+        cin >> x;
+        if(x == 42){
+            return 0;
+        }
+        cout << x << endl;
+    }
+}
