2770cb8a709a3130c086307cdaf9f19667bf4c04
jankrepl <jankrepl@public-docking-hg-5681.ethz.ch>
2017-04-26 14:00:04 +0200

    new

---
+++ Frequently_Used_Algorithms/__template.cpp
@@ -0,0 +1,37 @@
+// Algorithm: 
+// Author: Jan Krepl
+
+/* Libraries */
+#include <cmath>
+#include <cstdio>
+#include <set>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <map>
+#include <assert.h>
+#include <cstring>
+#include <tuple>
+
+using namespace std;
+
+
+/* Description */
+
+
+/* Tips and tricks */
+
+
+/* Example problem */
+
+
+/* Implementation #1 */
+
+
+/* Implementation #2 */
+
+
+/* Implementation #3 */
+
