abd42f02bebeca54fc4cf5dac63eaf29b55d2956
jankrepl <jankrepl@85-195-198-176.init7.net>
2017-05-13 19:51:42 +0200

    dsa

---
+++ Frequently_Used_Algorithms/Extended_Euclid_Algorithm.cpp
@@ -0,0 +1,58 @@
+// Algorithm: Extended_Euclidian_Algorithm
+// Author: Jan Krepl
+
+/* Libraries */
+#include <cmath>
+#include <cstdio>
+#include <set>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <map>
+#include <assert.h>
+#include <cstring>
+#include <tuple>
+
+using namespace std;
+
+
+/* Description */
+// Used for finding the unique solution (x,y) (x,y \in Z) of Bezouts identity
+// ax + by = gcd(a,b) ..where (a,b) given
+
+
+
+
+/* Complexity */
+
+
+/* Tips and tricks */
+
+
+
+/* Example problem */
+
+
+
+/* Implementation #1 */
+int extendedGCD(int a, int b, int &x, int &y) {
+    // everything divides 0
+    if(b == 0) {
+       x = 1;
+       y = 0;
+       return a;
+    }
+
+    int x1 = 0, y1 = 0, gcd = extendedGCD(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - (a / b) * y1;
+    return gcd;
+}
+
+/* Implementation #2 */
+
+
+/* Implementation #3 */
+
